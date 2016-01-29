c9897f5bbc7f2444a22289e0c6feea7511798a11
eugenelee456@gmail.com <eugenelee456@gmail.com>
2016-01-29 11:25:47 +0000

    Adding comments

--- Task1/Quiz.py
+++ Task1/Quiz.py
@@ -1,17 +1,17 @@
 import random
 __author__ = 'Eugene'
 
-OPERATOR_TYPES = ["+", "-", "*"]
-player_name = input("Please enter your name:")
-player_score = 0
-player_answer = 0
+OPERATOR_TYPES = ["+", "-", "*"] # Set the different types of questions possible
+player_name = input("Please enter your name:") # Ask the users for their names
+player_score = 0 # Initialize score
+player_answer = 0 # Initialize the player answer
 
-for count in range(10):
-    question_operator = random.choice(OPERATOR_TYPES)
-    first_number = random.randint(1, 12)
-    second_number = random.randint(1, 12)
-    print("What is {0} {1} {2}?".format(first_number, question_operator, second_number))
-    while(True):
+for count in range(10): # Ensure that 10 questions are asked
+    question_operator = random.choice(OPERATOR_TYPES) # Used random.choice rather than deriving a random index for clarity
+    first_number = random.randint(1, 12) # First number
+    second_number = random.randint(1, 12) # Second number
+    print("What is {0} {1} {2}?".format(first_number, question_operator, second_number)) # Ask the question
+    while(True): # Check that the answer is a valid number, otherwise program will crash when converting answer to integer
         try:
             player_answer = int(input("Your answer: "))
             break
@@ -19,17 +19,17 @@ for count in range(10):
             print("Please ensure you enter a number.")
 
 
-    if question_operator == "+":
+    if question_operator == "+": # Calculate the correct answer
         correct_answer = first_number + second_number
     elif question_operator == "-":
         correct_answer = first_number - second_number
     else:
         correct_answer = first_number * second_number
 
-    if correct_answer == player_answer:
-        player_score += 1
+    if correct_answer == player_answer: # See if the player got the answer correct.
+        player_score += 1 # Add 1 to the score if the player got the answer correct
         print("You got it correct!")
     else:
-        print("You got it wrong. Better luck next time!")
+        print("You got it wrong. Better luck next time!") # Do nothing to score if the player got the answer wrong.
 
-print("Well done! You got {0} out of 10!".format(player_score))
+print("Well done! You got {0} out of 10!".format(player_score)) # Tell the user what their final score was.
